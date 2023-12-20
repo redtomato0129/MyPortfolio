ecc69b9b2ad8bd1d6d92c5d792fb2b3ad572410e
redtomato0129 <redtomato0129@gmail.com>
2023-12-21 00:02:44 +0530

    codes Added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
+import leaf from "../../Assets/Projects/Never-More.png";
+import emotion from "../../Assets/Projects/Aryabhatta Academy.png";
+import editor from "../../Assets/Projects/editor.png";
+import chatify from "../../Assets/Projects/portfolio.png";
 import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import bitsOfCode from "../../Assets/Projects/suicide.png";
 
 function Projects() {
   return (
@@ -25,10 +25,10 @@ function Projects() {
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="50 Front-end Project"
+              description="This git repository consists of a collection of 50 carefully curated frontend projects that showcase your skills and creativity as a frontend developer. Each project is unique and visually stunning, demonstrating your proficiency in a wide range of technologies and design principles ."
+              ghLink="https://github.com/SudeepAcharjee/The-50-Front-end-Project"
+              // demoLink="https://chatify-49.web.app/"
             />
           </Col>
 
@@ -36,10 +36,10 @@ function Projects() {
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Portfolio"
+              description="Portfolio Web App: A dynamic GitHub repository showcasing a personalized web application for creating and managing professional portfolios. Tailored for individuals and creatives, it features an intuitive interface, customizable templates, and seamless integration to highlight skills, projects, and achievements in a visually compelling manner"
+              ghLink="https://github.com/SudeepAcharjee/Portfolio"
+              demoLink="https://sudeep-acharjee.netlify.app/"
             />
           </Col>
 
@@ -47,10 +47,10 @@ function Projects() {
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Photo Editor"
+              description="PhotoEditor Web App: An open-source GitHub repository offering a versatile online platform for image editing. Empowering users with a range of tools and filters, it provides a seamless and customizable experience for enhancing, retouching, and transforming photos directly in the web browser."
+              ghLink="https://github.com/SudeepAcharjee/Photo-Editor"
+              demoLink="https://photo-editor-30.netlify.app/"              
             />
           </Col>
 
@@ -58,10 +58,10 @@ function Projects() {
             <ProjectCard
               imgPath={leaf}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Never-More"
+              description="We create simplified and interactive learning experiences. Learning web development should be easy to understand and available for everyone, everywhere! Never-More is a school for web developers, covering all the aspects of web development."
+              ghLink="https://github.com/SudeepAcharjee/Never_more"
+              demoLink="https://never-more.netlify.app/"
             />
           </Col>
 
@@ -69,10 +69,10 @@ function Projects() {
             <ProjectCard
               imgPath={suicide}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              title="Attendance System"
+              description="Group Project made by bca 3rd sem students. Attendance web app for tracking and managing attendance, simplifying record-keeping and enhancing organizational efficiency. A website for IT department."
+              ghLink="https://github.com/SudeepAcharjee/LCB-IT-dept"
+              demoLink="https://lcbc-it-dept.netlify.app/"
             />
           </Col>
 
@@ -80,11 +80,10 @@ function Projects() {
             <ProjectCard
               imgPath={emotion}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="Online-Automated School Website"
+              description="This is one of my freelancing project...A full-stack webapp for a school environment.This the not the real document ..its just a sample of the project. This is one my freelancing Project"
+              ghLink="https://github.com/SudeepAcharjee/Aryabhatta-Academy"
+              demoLink="https://check-school.netlify.app/"      
             />
           </Col>
         </Row>
