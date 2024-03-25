5fc7b86c19c3e6952346cb9d799d50f6a8e9f3a6
redtomato0129 <redtomato0129@gmail.com>
2024-03-25 07:36:49 -0400

    confilct 06

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -40,7 +40,7 @@ function NavBar() {
       <Container>
         <Navbar.Brand href="/" className="d-flex">
           {/* <img src={logo} className="img-fluid logo" alt="brand" /> */}
-          <p>Sudeep</p>
+          <p>Super</p>
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
